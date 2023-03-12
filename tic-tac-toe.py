e65e1185d164726678438db052f2f81cda0e09bb
Fernando <fernandochaza@gmail.com>
2023-03-12 11:43:01 -0300

    First mockup

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,2 +1,41 @@
 import tkinter as tk
 import random
+
+
+# Declare/Initialize global variables needed
+
+
+
+# Define main function
+def main():
+    pass
+
+
+# Define create_board function
+def create_board():
+    pass
+
+
+# Define restart_game function
+def restart_game():
+    pass
+
+
+# Define check_win function
+def check_win():
+    pass
+
+
+# Define check_tie function
+def check_tie():
+    pass
+
+
+# Define check_empty_spaces_in_board function
+def check_empty_spaces_in_board():
+    pass
+
+
+# Run program with if statement for testing purposes
+if __name__ == "__main__":
+    main()
