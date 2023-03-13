0629ce403a2bdf63aee38f14434f5cd3b32c531f
Fernando <fernandochaza@gmail.com>
2023-03-13 08:54:42 -0300

    Refactor add_mark function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -26,8 +26,7 @@ def create_dynamic_header():
     """Create a header that will display player's
     turns and game messages
     """
-    # Make the header variable accessible for other functions
-    global header_label
+    global header_label  # Make the header variable accessible for other functions
 
     header_label = ttk.Label(
         text=f"Player {actual_player} turn",
@@ -35,17 +34,16 @@ def create_dynamic_header():
         font=("Calibri", 30, "bold"),
         foreground="#0062B8",
     )
-
-    # Set top and bottom margins of header and center it
-    header_label.pack(pady=(10, 20), anchor="center")
+    header_label.pack(
+        pady=(10, 20), anchor="center"
+    )  # Set top/ bottom margins and center it
 
 
 def create_board():
     board_frame = tk.Frame()
     board_frame.pack()
 
-    # Create the board buttons for each row and column
-    for row in range(3):
+    for row in range(3):  # Create the board buttons for each row and column
         for column in range(3):
             board_buttons[row][column] = tk.Button(
                 board_frame,
@@ -56,31 +54,41 @@ def create_board():
                 height=2,
                 command=lambda row=row, column=column: add_mark(row, column),
             )
-            # Create the board buttons for each row/column combination
-            board_buttons[row][column].grid(row=row, column=column)
+            board_buttons[row][column].grid(
+                row=row, column=column
+            )  # Display each button in grid
 
 
 def add_mark(row, column):
-    """_summary_
+    """Check if the space is empty and if the game has finished
+        before adding the actual player mark to the board.
+        Then, check if the player wins to display the winning message
+        in the header or change the player turn accordingly
 
     Args:
-        row (_type_): _description_
-        column (_type_): _description_
+        row (int): Board's row to place the mark on.
+        column (int): Board's column to place the mark on.
     """
-    global actual_player
-    player2 = "⭕️" if actual_player == "❌" else "❌"
+    global actual_player  # Access the actual_player variable
+
+    player2 = (
+        players[1] if actual_player == players[0] else players[0]
+    )  # Get the other player's mark value
 
     if board_buttons[row][column]["text"] == "" and check_win() is False:
-        if actual_player == players[0]:
+        if actual_player == players[0]:  # Set mark color to players
             board_buttons[row][column].configure(foreground="red")
         else:
             board_buttons[row][column].configure(foreground="blue")
+
+        # Display the actual player's mark value
         board_buttons[row][column]["text"] = actual_player
 
         if check_win() is True:
             header_label.configure(text=f"Player {actual_player} WINS!")
 
-        elif check_win() is False and check_empty_spaces_in_board() is True:
+        elif check_empty_spaces_in_board() is True:
+            # Change turns
             actual_player = player2
             header_label.configure(text=f"Player {actual_player} turn")
 
@@ -88,9 +96,7 @@ def add_mark(row, column):
             header_label.configure(text="Is a TIE!")
 
 
-# Define restart_game function
 def create_restart_game_button():
-    """_summary_"""
     style = ttk.Style()
     style.configure(
         "restart_game_button.TButton",
